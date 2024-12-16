7df5d7eb7548ec8256935a95fbc0dbe8d4c582cc
anabolicobsession <pilvorm@gmail.com>
2024-12-16 21:03:50 +0100

    Feat: Add displaying uptime in bot description

--- dex_sonar/main.py
+++ dex_sonar/main.py
@@ -1,6 +1,7 @@
 import logging
 from asyncio import CancelledError, sleep
 
+from dex_sonar import time
 from dex_sonar.bot import Bot
 from dex_sonar.config import BOT_TOKEN, SILENT_BOT_TOKEN
 from dex_sonar.live_pairs import LivePairs
@@ -21,6 +22,7 @@ class Application:
             include_filter=lambda pairs: sorted(pairs, key=lambda x: x.turnover, reverse=True)[:1],
             callback_on_update=self.callback_on_pair_update,
         )
+        self.start = time.get_timestamp()
 
     def run(self):
         self.bot.run(self._run())
@@ -28,7 +30,9 @@ class Application:
     async def _run(self):
         try:
             self.pairs.subscribe_to_stream()
-            while True: await sleep(1e6)
+            while True:
+                await self.bot.set_description(f'Uptime: {time.format_timedelta(time.get_time_passed_since(self.start), shorten=True)}')
+                await sleep(60)
 
         except CancelledError:
             logger.info(f'Stopping the bot')
