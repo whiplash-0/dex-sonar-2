c8a063b275d1049f895a0fd2a2e9e78de7e276da
anabolicobsession <pilvorm@gmail.com>
2024-12-29 12:37:18 +0100

    Ops(scripts): Remove setting pause scheduler var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,4 @@
 
 heroku config:set $(tr '\n' ' ' <<< "$(cat .env)")
 git push -f heroku "$1":main
-
 heroku ps:scale worker=1
-heroku config:set PAUSE_SCHEDULER=false
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 heroku ps:scale worker=0
-heroku config:set PAUSE_SCHEDULER=true
