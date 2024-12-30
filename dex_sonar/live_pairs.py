89b3a81141a75b66d895e5bc700fce27ac286dd1
anabolicobsession <pilvorm@gmail.com>
2024-12-30 18:53:22 +0100

    Refactor: Separate pairs entity from live pairs

--- dex_sonar/live_pairs.py
+++ dex_sonar/live_pairs.py
@@ -1,40 +1,29 @@
 from contextlib import contextmanager
 from datetime import datetime, timedelta
-from typing import Callable, Iterable, Iterator
+from typing import Callable, Iterable
 
 from pybit.unified_trading import HTTP, WebSocket
 
 from dex_sonar import time
 from dex_sonar.pair import Pair, Symbol, TimeSeries
+from dex_sonar.pairs import Pairs
 from dex_sonar.pybit_converters import Response, convert_get_kline, convert_get_tickers, convert_stream_kline, convert_stream_ticker
 
 
-class LivePairs:
+class LivePairs(Pairs):
     def __init__(
             self,
             update_frequency: timedelta = timedelta(seconds=10),
             callback_on_update: Callable[[Pair], None] = lambda _: None,
             include_filter: Callable[[list[Pair]], Iterable[Pair]] = lambda pairs: sorted(pairs, key=lambda x: x.turnover, reverse=True)[:10],
     ):
-        self.pairs: dict[Symbol, Pair] = {}
+        super().__init__()
         self.include_filter = include_filter
         self.update_frequency = update_frequency
         self.callback_on_update = callback_on_update
         self.last_update: dict[Symbol, datetime] = {}
         self.requests = HTTP(testnet=False)
         self._init()
-
-    def __len__(self):
-        return len(self.pairs)
-
-    def __iter__(self) -> Iterator[Pair]:
-        return iter(self.pairs.values())
-
-    def __getitem__(self, key) -> Pair:
-        return self.pairs[key]
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({", ".join([x.pretty_symbol for x in self])})'
 
     @contextmanager
     def subscribe_to_stream(self):
@@ -46,8 +35,8 @@ class LivePairs:
                 channel_type='linear',
             )
             self._update_klines()
-            ws.kline_stream(1, self.pairs.keys(), self._handle_kline_update)
-            ws.ticker_stream(self.pairs.keys(), self._handle_ticker_update)
+            ws.kline_stream(1, self.get_symbols(), self._handle_kline_update)
+            ws.ticker_stream(self.get_symbols(), self._handle_ticker_update)
             yield
 
         finally:
@@ -67,13 +56,14 @@ class LivePairs:
                 next_funding_time=ticker.next_funding_time,
             ))
 
-        self.pairs = {x.symbol: x for x in self.include_filter(pairs)}
+        self.update(self.include_filter(pairs))
         self.last_update = {x: time.MIN_TIMESTAMP for x in self.pairs}
 
     def _update_klines(self):
-        for symbol in self.pairs: self._update_kline(symbol)
+        for symbol in self.get_symbols(): self._update_kline(symbol)
 
     def _update_kline(self, symbol: Symbol):
+        pair = self[symbol]
         kline = convert_get_kline(
             self.requests.get_kline(
                 category='linear',
@@ -85,36 +75,36 @@ class LivePairs:
         )
 
         # confirmed (closed) candles
-        self.pairs[symbol].prices.update(
+        pair.prices.update(
             kline.closes[:-1],
             kline.starts[1:],
             is_final=True,
         )
-        self.pairs[symbol].turnovers.update(
+        pair.turnovers.update(
             kline.turnovers[:-1],
             kline.starts[1:],
             is_final=True,
         )
 
         # last unconfirmed (unknown status) candle
-        self.pairs[symbol].prices.update(
+        pair.prices.update(
             kline.closes[-1],
-            kline.starts[-1] + self.pairs[symbol].prices.get_time_step(),
+            kline.starts[-1] + pair.prices.get_time_step(),
         )
-        self.pairs[symbol].turnovers.update(
+        pair.turnovers.update(
             kline.turnovers[-1],
-            kline.starts[-1] + self.pairs[symbol].turnovers.get_time_step(),
+            kline.starts[-1] + pair.turnovers.get_time_step(),
         )
 
     def _handle_kline_update(self, response: Response):
         if response['data'][0]['confirm']:
             kline = convert_stream_kline(response)
-            self.pairs[kline.symbol].prices.update(
+            self[kline.symbol].prices.update(
                 kline.close,
                 time.ceil_timestamp_minute(kline.end),
                 is_final=True,
             )
-            self.pairs[kline.symbol].turnovers.update(
+            self[kline.symbol].turnovers.update(
                 kline.turnover,
                 time.ceil_timestamp_minute(kline.end),
                 is_final=True,
@@ -126,7 +116,7 @@ class LivePairs:
         if time.get_timestamp() - self.last_update[symbol] >= self.update_frequency:
             self.last_update[symbol] = time.get_timestamp()
             ticker = convert_stream_ticker(response)
-            pair = self.pairs[symbol]
+            pair = self[symbol]
 
             pair.prices.update(
                 ticker.price,
