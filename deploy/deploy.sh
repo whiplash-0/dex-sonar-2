e3e378993a781e67021642143d85b2ec19066915
anabolicobsession <pilvorm@gmail.com>
2025-03-29 17:33:58 +0100

    Refactor(deploy): Rename scripts for convenience

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+scripts_dir="deploy"
+
 project_root_path="$(dirname "$(realpath "$0")")/.."
 
 if cd "$project_root_path"; then  # move to project level directory for reproducibility
   git add *
   git commit -m "Ops: Temporal commit for Heroku deployment"
-  ./scripts/deploy.sh "$(git branch --show-current)"
+  bash "$scripts_dir/branch_deploy.sh" "$(git branch --show-current)"
   git reset --soft HEAD~1
   git reset
 else
