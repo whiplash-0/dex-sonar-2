dd5bfc7a8bc4a43dcac121104a521f1466534dc1
anabolicobsession <pilvorm@gmail.com>
2025-04-05 16:12:37 +0200

    Fix(deploy): Handle deploy case when there are no changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,10 +6,13 @@ project_root_path="$(dirname "$(realpath "$0")")/.."
 
 if cd "$project_root_path"; then  # move to project level directory for reproducibility
   git add *
-  git commit -m "Ops: Temporal commit for Heroku deployment"
+  are_there_changes=$(git diff --cached --quiet || echo true)
+  [ "$are_there_changes" = "true" ] && git commit -m "Ops: Temporal commit for Heroku deployment"
+
   bash "$scripts_dir/branch_deploy.sh" "$(git branch --show-current)"
-  git reset --soft HEAD~1
-  git reset
+
+  [ "$are_there_changes" = "true" ] && git reset --soft HEAD~1
+  [ "$are_there_changes" = "true" ] && git reset
 else
     echo "Failed to change directory to project root"
     exit 1
