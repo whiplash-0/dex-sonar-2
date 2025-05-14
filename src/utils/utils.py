9765673ba5d4cc2f3d8ada5c10ca7fe7b8a1d9b5
anabolicobsession <pilvorm@gmail.com>
2025-05-14 18:27:56 +0200

    Chore(utils): Add `NumericUnit`

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,45 @@
 import math
+from enum import IntEnum, auto
+
+
+
+class NumericUnit(IntEnum):
+    def _generate_next_value_(name, start, count, last_values):
+        return 10 ** (3 * count)
+
+    ONE = auto()
+    THOUSAND = auto()
+    MILLION = auto()
+    BILLION = auto()
+    TRILLION = auto()
+    QUADRILLION = auto()
+
+
+
+UNIT_MAPPING = {
+    'K': NumericUnit.THOUSAND,
+    'M': NumericUnit.MILLION,
+    'B': NumericUnit.BILLION,
+    'T': NumericUnit.TRILLION,
+    'Q': NumericUnit.QUADRILLION,
+}
+UNIT_LETTERS = list(UNIT_MAPPING.keys())
+
+
+def format_large_number(x, decimal_places=0, decrease_decimal_places=False):
+    if abs(x) < 1000:
+        return f'{(int(x))}'
+    else:
+        unit = int(math.log10(abs(x))) // 3
+        n = x / 10 ** (3 * unit)
+        integer_digits = len(str(int(n)))
+        final_decimal_places = decimal_places if not decrease_decimal_places else max(decimal_places - integer_digits + 1, 0)
+        return f'{n:.{final_decimal_places}f}{UNIT_LETTERS[unit - 1]}'
+
+
+def parse_large_number(string, as_type: type[int] | type[float] = float) -> int | float:
+    return as_type(string) if not string[-1].isalpha() else as_type(string[:-1]) * UNIT_MAPPING[string[-1]]
+
 
 
 def format_number_by_significant_digits(x, significant_digits=1, decimal_places=0, keep_leading_zeros=False):
@@ -16,26 +57,3 @@ def format_number_by_significant_digits(x, significant_digits=1, decimal_places=
             string = integer_part + ('.' + decimal_part if decimal_part else '')
 
         return string
-
-
-UNIT_MAPPING = {
-    'K': 10 ** 3,
-    'M': 10 ** 6,
-    'B': 10 ** 9,
-    'T': 10 ** 12,
-    'Q': 10 ** 15,
-}
-UNIT_LETTERS = list(UNIT_MAPPING.keys())
-
-def format_large_number(x, decimal_places=0, decrease_decimal_places=False):
-    if abs(x) < 1000:
-        return f'{(int(x))}'
-    else:
-        unit = int(math.log10(abs(x))) // 3
-        n = x / 10 ** (3 * unit)
-        integer_digits = len(str(int(n)))
-        final_decimal_places = decimal_places if not decrease_decimal_places else max(decimal_places - integer_digits + 1, 0)
-        return f'{n:.{final_decimal_places}f}{UNIT_LETTERS[unit - 1]}'
-
-def parse_large_number(string, as_type: type[int] | type[float] = float) -> int | float:
-    return as_type(string) if not string[-1].isalpha() else as_type(string[:-1]) * UNIT_MAPPING[string[-1]]
